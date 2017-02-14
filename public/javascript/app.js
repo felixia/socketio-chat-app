fb1299ab174d6a344ed27c9df667d1b27e382bcf
Felix Olonde <olondefelix@yahoo.com>
2017-02-14 15:32:26 +0300

    Front-end form

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -9,3 +9,14 @@ socket.on('message', function (message) {
     console.log(message.text);
 });
 
+//handle submitting of new message
+var $form = jQuery('#message-form');
+$form.on('submit', function (event) {
+    event.preventDefault();
+
+    socket.emit('message', {
+        text: $form.find('input[name=message]').val()
+    });
+    $form.find('input[name=message]').val('');
+});
+
